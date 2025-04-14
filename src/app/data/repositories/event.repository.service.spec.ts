c0d9decfb3caa596151f100897f3672ff910df5a
josemaria_yañez <102536119+merikorg@users.noreply.github.com>
2025-04-14 12:35:36 -0500

    ✨ Creating services and home components

---
+++ src/app/data/repositories/event.repository.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { EventRepositoryService } from './event.repository.service';
+
+describe('EventRepositoryService', () => {
+  let service: EventRepositoryService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(EventRepositoryService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
