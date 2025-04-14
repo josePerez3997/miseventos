2f3181e8391f5d97d937428f68e3335763ee08e7
josemaria_yañez <102536119+merikorg@users.noreply.github.com>
2025-04-14 14:57:34 -0500

    🔧 routing module to routes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app-routing.module';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
